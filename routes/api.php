6a2184104c1bf963d9bb65d12871755a76b3fb01
Pierre Silva <pierremichelsilva@gmail.com>
2020-10-02 18:09:09 -0500

    Simple api test

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::any('test', function (Request $request) {
+  return response()->json([
+    'message' => 'Hello world!',
+    'data' => $request->all()
+  ]);
+});
